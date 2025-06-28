0234495919083105cc0a5509d927e4fb7f22ab99
Jarin-Tanzim <jarin261900@gmail.com>
2025-06-28 11:59:00 +0600

    Add files via upload

---
+++ src/pkg223071132_project/HomepageController.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package pkg223071132_project;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+
+public class HomepageController implements Initializable {
+
+    @FXML
+    private Button AllProducts;
+    @FXML
+    private Button Dress;
+    @FXML
+    private Button Jewllery;
+    @FXML
+    private Button Bags;
+    @FXML
+    private Button Shoes;
+    @FXML
+    private Button logout;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void HandleAllProducts(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandleDress(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandleJewllery(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandleBags(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandleShoes(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandleLogout(ActionEvent event) {
+    }
+    
+}
