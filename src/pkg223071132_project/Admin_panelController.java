0a8d376acad6b9361ab50b148d4a5bb3db4817dd
Jarin-Tanzim <jarin261900@gmail.com>
2025-06-30 01:23:09 +0600

    Add files via upload

---
+++ src/pkg223071132_project/Admin_panelController.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package pkg223071132_project;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author Apollo Gadget
+ */
+public class Admin_panelController implements Initializable {
+
+    @FXML
+    private Button adminLogout;
+    @FXML
+    private Button ViewUserButton;
+    @FXML
+    private Button AddProductsButton;
+    @FXML
+    private Button ViewCardButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleAdminLogout(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandleUsers(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandleAddProducts(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleViewCart(ActionEvent event) {
+    }
+    
+}
