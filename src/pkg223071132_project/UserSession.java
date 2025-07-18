eb963733cad9e61ce7efc62ea8422d3d59cf5585
Jarin-Tanzim <jarin261900@gmail.com>
2025-07-18 15:39:49 +0600

    Updated UserSession.java to handle recent updates

--- src/pkg223071132_project/UserSession.java
+++ src/pkg223071132_project/UserSession.java
@@ -2,16 +2,30 @@ package pkg223071132_project;
 
 public class UserSession {
     private static boolean loggedIn = false;
+    private static int loggedInUserId = -1;
+    private static String role = "";  
 
     public static boolean isLoggedIn() {
         return loggedIn;
     }
 
-    public static void login() {
+    public static void login(int userId, String userRole) {
         loggedIn = true;
+        loggedInUserId = userId;
+        role = userRole;
     }
 
     public static void logout() {
         loggedIn = false;
+        loggedInUserId = -1;
+        role = "";
+    }
+
+    public static int getLoggedInUserId() {
+        return loggedInUserId;
+    }
+
+    public static String getRole() {
+        return role;
     }
 }
