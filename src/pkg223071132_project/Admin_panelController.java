b9d97205148e504d24d4441daa587f55617ad5d4
Jarin-Tanzim <jarin261900@gmail.com>
2025-07-18 15:46:47 +0600

    Updates Admin_panelController.java to handle view users

--- src/pkg223071132_project/Admin_panelController.java
+++ src/pkg223071132_project/Admin_panelController.java
@@ -4,12 +4,18 @@
  */
 package pkg223071132_project;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -36,11 +42,52 @@ public class Admin_panelController implements Initializable {
     }    
 
     @FXML
-    private void handleAdminLogout(ActionEvent event) {
+
+private void handleAdminLogout(ActionEvent event) {
+    try {
+        
+        UserSession.logout();
+
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("homepage.fxml"));
+        Parent root = loader.load();
+
+        HomepageController controller = loader.getController();
+        controller.setLoggedIn(false, -1, "");
+
+       
+        Stage newStage = new Stage();
+        newStage.setScene(new Scene(root));
+        newStage.setTitle("Home Page");
+        newStage.show();
+
+       
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
+
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+}
+
 
     @FXML
     private void HandleUsers(ActionEvent event) {
+        try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view_users.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setTitle("User List");
+        stage.setScene(new Scene(root));
+        stage.show();
+        
+        
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
+        
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
     }
 
     @FXML
